54b458775c0810c3501d5cfcfaf484386b9ac3ad
jobinsjose92 <50811203+jobinsjose92@users.noreply.github.com>
2022-03-28 06:53:10 -0500

    first commit

---
+++ app/src/main/java/com/jobins/firebaselogin/RegisterActivity.java
@@ -0,0 +1,25 @@
+package com.jobins.firebaselogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class RegisterActivity extends AppCompatActivity {
+
+
+    private EditText email;
+    private EditText password;
+    private Button register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        register = findViewById(R.id.register);
+    }
+}
