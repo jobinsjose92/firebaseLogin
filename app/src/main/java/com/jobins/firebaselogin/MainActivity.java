5a3c2b5663aa0878184cfdcf36e456b8e785c3b0
jobinsjose92 <50811203+jobinsjose92@users.noreply.github.com>
2022-03-31 20:58:02 -0500

    third commit

--- app/src/main/java/com/jobins/firebaselogin/MainActivity.java
+++ app/src/main/java/com/jobins/firebaselogin/MainActivity.java
@@ -6,38 +6,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button register;
-    private Button login;
+private Button logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
-        register = findViewById(R.id.register);
-        login = findViewById(R.id.login);
+        logout = findViewById(R.id.logout);
 
-        register.setOnClickListener(new View.OnClickListener() {
+        logout.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
-                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
-
-            finish();
-        }
-        });
-
-
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v)
-            {
-                startActivity(new Intent(MainActivity.this, LoginActivity.class));
-
-                finish();
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Toast.makeText(MainActivity.this, "logged out",Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MainActivity.this, StartActivity.class));
             }
         });
 
